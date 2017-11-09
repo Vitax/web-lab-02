70049d09d5f6b09eb5dacd1788335f3690cfb256
vitax <caglar.oezel@gmail.com>
2017-11-09 14:11:31 +0100

    working greyscaling of image and change event of brightness

--- src/image_processing.js
+++ src/image_processing.js
@@ -1,14 +1,55 @@
 $(document).ready(function() {
-    var img = new Image();
-    var canvas = $("#myCanvas")[0];
-    var context = canvas.getContext("2d");
-  
-    img.addEventListener("load", function() {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      context.drawImage(img, 0, 0);
-      var data = context.getImageData(0, 0, img.width, img.height);
-    });
-    img.src = "./assets/images/lena_std.png";
+  let originalImage = [];
+  let median = 0;
+  let image = new Image();
+  image.src = "./assets/images/lena_std.png";
+
+  let canvas = $("#myCanvas")[0];
+  canvas.width = image.width;
+  canvas.height = image.height;
+  let context = canvas.getContext("2d");
+  greyScaleImage(image, originalImage, context, median);
+  modifyBrithness(image, originalImage, context, median);
+})
+
+function greyScaleImage(image, originalImage, context, median) {
+  image.addEventListener("load", function () {
+    context.drawImage(image, 0, 0);
+
+    let imageData = context.getImageData(0, 0, image.width, image.height);
+    for(let y = 0; y < imageData.height; y++){
+      for(let x = 0; x < imageData.width; x++){
+          let i = (y * 4) * imageData.width + x * 4;
+          let avg = (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3;
+
+          imageData.data[i] = avg; 
+          originalImage[i] = avg; 
+          
+          imageData.data[i + 1] = avg;
+          originalImage[i + 1] = avg;  
+          
+          imageData.data[i + 2] = avg;
+          originalImage[i + 2] = avg;
+      }
+    }
+    context.putImageData(imageData, 0, 0);
   });
+}
+
+function modifyBrithness(image, originalImage, context, median) {
   
+  $(document).on("input change", '#brightnessSlider', function() {
+    let valueOfSlider = $(this).val();
+    let imageData = context.getImageData(0,0, image.width, image.height);
+
+    for(let i = 0; i < originalImage.length; i+= 4) {;
+        let brightness = originalImage[i] + Number(valueOfSlider);
+  
+        imageData.data[i] = brightness;
+        imageData.data[i + 1] = brightness;
+        imageData.data[i + 2] = brightness;
+    }
+
+    context.putImageData(imageData, 0, 0);
+  })
+}
